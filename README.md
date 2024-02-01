4d36df5d9a51009a2ee1429a9587aa754b731403
Ivo Nunes <inunes1904@gmail.com>
2024-02-01 23:06:39 +0000

    Configuration Done!

--- README.md
+++
@@ -1,1 +0,0 @@
-# Learning Ressources Small Project
---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Config DOne Lets GO!</h1>
+</template>
+
+<script>
+
+export default{
+    data(){
+        return{
+
+    };
+    }
+}
+</script>
